5cd3650dd003e3dd8566d60718bdec985b06f660
Turza <tpodder191239@bscse.uiu.ac.bd>
2022-01-08 19:28:48 +0600

    Singleton Pattern Implemented

--- SWEassignment/src/TestClass.java
+++ SWEassignment/src/TestClass.java
@@ -10,7 +10,28 @@ public class TestClass {
     public static void main(String[] args) {
         TestClass tc = new TestClass(11, "Anisur");
 
+        System.out.println("Student ID: "+ tc.id+" Name: "+tc.name);
+        TestClass2  X= TestClass2.getInstance();
+        System.out.println(X.s);
+
     }
 
+}
+class TestClass2 {
+    private static TestClass2 ins = null;
+
+    public String s;
+    private TestClass2()
+    {
+        s = "This a singleton Test Class";
+    }
+
+    public static TestClass2 getInstance()
+    {
+        if (ins == null)
+            ins = new TestClass2();
+
+        return ins;
+    }
 }
 
